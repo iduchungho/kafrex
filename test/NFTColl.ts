d5194547b1a0630b6c538d778c34db822196a101
Kawasaki <iduchungho@gmail.com>
2023-06-09 17:59:04 +0700

    smc 3

--- test/NFTColl.ts
+++ test/NFTColl.ts
@@ -48,7 +48,7 @@ describe('Kafrex NFT Collection', () => {
             expect(await kafToken.ownerOf(tokenID2)).to.equal(addr2.address);
         })
 
-        it("transfer NFT", async () => {
+        it("check transfer NFT count", async () => {
             const {kafToken, nftColl, owner, addr1, addr2} = await loadFixture(deployNFTCollection)
 
             await kafToken.mint(owner.address, "https://token-uri-1.com");
@@ -63,6 +63,25 @@ describe('Kafrex NFT Collection', () => {
             await nftColl.transferNFT(addr2.address, tokenID2)  
             expect(await kafToken.ownerOf(tokenID1)).to.equal(addr1.address);
             expect(await kafToken.ownerOf(tokenID2)).to.equal(addr2.address);
+
+            expect(await nftColl.getNFTCount()).to.equal(0);
+        })
+
+        it("check transfer NFT link", async () => {
+            const {kafToken, nftColl, owner, addr1, addr2} = await loadFixture(deployNFTCollection)
+
+            await kafToken.mint(owner.address, "https://token-uri-1.com");
+            const tokenID1 = await kafToken.getTokenID();
+
+            await kafToken.mint(owner.address, "https://token-uri-2.com")
+            const tokenID2 = await kafToken.getTokenID();
+
+            await nftColl.addNFT(tokenID1)
+            await nftColl.addNFT(tokenID2)
+            await nftColl.transferNFT(addr1.address, tokenID1)
+            await nftColl.transferNFT(addr2.address, tokenID2)  
+            expect(await kafToken.tokenURI(tokenID1)).to.equal("https://token-uri-1.com");
+            expect(await kafToken.tokenURI(tokenID2)).to.equal("https://token-uri-2.com");
 
             expect(await nftColl.getNFTCount()).to.equal(0);
         })
